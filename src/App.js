7535ef9e74a626c5005aec8a69bcdcc15bb84dc2
Huy <huytranquang.150701@gmail.com>
2023-02-14 23:20:44 +0700

    upload frontend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Header from "./Components/Header/Header";
+import Edit from "./Components/Edit/Edit";
+import { useSelector } from "react-redux";
+import Footer from "./Components/Footer/Footer";
+import Makepost from "./Components/Posts/Makepost";
+import Post from "./Components/Posts/Post";
 
 function App() {
+  const [isEdit, setEdit] = useState(false);
+  const [isOpenPost, setOpen] = useState(false);
+  const pending = useSelector((state) => state.user.pending);
+  const error = useSelector((state) => state.user.error);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {isEdit ? (
+        <Edit setEdit={setEdit} />
+      ) : !isEdit && !isOpenPost ? (
+        <>
+          <Header setEdit={setEdit} />
+          <div className="post-container">
+            <Post />
+          </div>
+          <Footer isOpenPost={isOpenPost} setOpen={setOpen} />
+        </>
+      ) : (
+        <>
+          <Header setEdit={setEdit} />
+          <Makepost setOpen={setOpen} />
+        </>
+      )}
+
+      {pending && <p className="loading">Loading...</p>}
+      {!isEdit && error && (
+        <p className="error">Error when is fetching data from server</p>
+      )}
     </div>
   );
 }
